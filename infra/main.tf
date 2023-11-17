3a1a5b37f558b32a7d352182777f3caaa0e7c1f6
mariusflores <mfloresdal@gmail.com>
2023-11-17 16:25:23 +0000

    added variables.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 resource "aws_apprunner_service" "service" {
-  service_name = "kjell-is-king"
+  service_name = ${var.service_name}
 
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
