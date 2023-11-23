c0087c2211c1c522f57c3fe98ed85993500aaa04
mariusflores <mfloresdal@gmail.com>
2023-11-23 16:23:45 +0000

    Some changes

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,7 +5,7 @@ variable "service_name"{
 
 variable "image_identifier"{
     type = string
-    default = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/2039-ecr-repo"
+    default = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/2039-ecr-repo:latest"
 }
 
 variable "iam_role_name"{
