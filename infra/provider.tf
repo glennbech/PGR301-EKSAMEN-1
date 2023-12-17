eb5754ad6d35e53f10bc5389e0133ca0deb1999c
glennbech <glenn.bech@gmail.com>
2023-12-17 14:58:00 +0100

    go docker & terr

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
  backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "candidate-2039/s3-bucket.state"
+    key    = "candidate-2039/s3-bucket-sensur.state"
     region = "eu-north-1"
  }
 }
